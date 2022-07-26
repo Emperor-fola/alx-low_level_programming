40a30d2e6bb6f86686595af6aa2c4459f5a25b98
Emperor-fola <fowotadefolarin@gmail.com>
2022-07-26 16:34:11 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,39 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - print an integer
- * @n: the integer to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-if (n > -1)
-{
-if (n > 9)
-print_number(n / 10);
-_putchar('0' + n % 10);
-}
-else
-{
-_putchar('-');
-if (n < -9)
-print_number(n / -10);
-_putchar('0' - n % 10);
-}
-}
-
-/**
- * main - print the number of arguments received
- * @argc: size of the argument vector
- * @argv: program name and arguments
- *
+ * main - main function with args
+ * @argc: Number of args passed
+ * @argv: Array containing all strings passed
  * Return: Always 0
  */
+
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-print_number(argc - 1);
-_putchar('\n');
+	int i, num;
 
-return (0);
+	for (i = 1; i < argc; i++)
+	{
+		num++;
+	}
+	printf("%d\n", num);
+	return (0);
 }
