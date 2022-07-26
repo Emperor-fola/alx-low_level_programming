a9a3aebbeca60ca718ed1f79a6bd99499d38698d
Emperor-fola <fowotadefolarin@gmail.com>
2022-07-26 16:35:34 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,30 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - print a string, followed by a new line
- * @str: the string to print
- *
- * Return: void
- */
-void _puts(char *str)
-{
-while (*str)
-_putchar(*str++);
-_putchar('\n');
-}
-
-/**
- * main - print the program name and arguments
- * @argc: size of the argument vector
- * @argv: program name and arguments
- *
+ * main - main function with args
+ * @argc: Number of args passed
+ * @argv: Array containing all strings passed
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{
-do {
-_puts(*argv++);
-} while (--argc > 0);
 
-return (0);
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	int i, num;
+
+	for (i = 1; i < argc; i++)
+	{
+		num++;
+	}
+	printf("%d\n", num);
+	return (0);
 }
